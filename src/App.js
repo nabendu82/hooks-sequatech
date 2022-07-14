3aafaaff2bade12309a33b0ee79298a321b0d17a
nabendu82 <nabendu.biswas@gmail.com>
2022-07-14 11:48:49 +0530

    First part

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import HookCounter4 from './components/HookCounter4';
+
+function App() {
+  return (
+    <div className="App">
+      <HookCounter4 />
+    </div>
+  );
+}
+
+export default App;
